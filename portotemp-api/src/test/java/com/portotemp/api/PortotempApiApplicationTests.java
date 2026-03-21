5021fe0c2603a2e6beedd025f393f2d7d9eda3f1
Francisco Lima <franciscosouzalima@gmail.com>
2026-03-21 11:01:30 -0300

    chore: estrutura inicial do projeto portotemp

---
+++ portotemp-api/src/test/java/com/portotemp/api/PortotempApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portotemp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortotempApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
